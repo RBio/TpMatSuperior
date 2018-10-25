c6648dafb9a14e60911d646e35b4fcc46e139af5
RBio <bio.ronan@gmail.com>
2018-10-25 15:34:42 -0300

    Generador de matriz

--- TpSuperior/Form1.Designer.cs
+++ TpSuperior/Form1.Designer.cs
@@ -28,21 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGenerateMatrix = new System.Windows.Forms.Button();
+            this.dgvMatrix = new System.Windows.Forms.DataGridView();
+            this.txtMatrixN = new System.Windows.Forms.NumericUpDown();
+            this.txtMatrixM = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatrixN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatrixM)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGenerateMatrix
+            // 
+            this.btnGenerateMatrix.Location = new System.Drawing.Point(713, 23);
+            this.btnGenerateMatrix.Name = "btnGenerateMatrix";
+            this.btnGenerateMatrix.Size = new System.Drawing.Size(75, 50);
+            this.btnGenerateMatrix.TabIndex = 0;
+            this.btnGenerateMatrix.Text = "Generar matriz";
+            this.btnGenerateMatrix.UseVisualStyleBackColor = true;
+            this.btnGenerateMatrix.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvMatrix
+            // 
+            this.dgvMatrix.AllowUserToAddRows = false;
+            this.dgvMatrix.AllowUserToDeleteRows = false;
+            this.dgvMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatrix.Location = new System.Drawing.Point(13, 92);
+            this.dgvMatrix.Name = "dgvMatrix";
+            this.dgvMatrix.Size = new System.Drawing.Size(775, 231);
+            this.dgvMatrix.TabIndex = 3;
+            this.dgvMatrix.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.matrix_CellContentClick);
+            // 
+            // txtMatrixN
+            // 
+            this.txtMatrixN.Location = new System.Drawing.Point(595, 23);
+            this.txtMatrixN.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.txtMatrixN.Name = "txtMatrixN";
+            this.txtMatrixN.Size = new System.Drawing.Size(112, 20);
+            this.txtMatrixN.TabIndex = 36;
+            // 
+            // txtMatrixM
+            // 
+            this.txtMatrixM.Location = new System.Drawing.Point(595, 53);
+            this.txtMatrixM.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.txtMatrixM.Name = "txtMatrixM";
+            this.txtMatrixM.Size = new System.Drawing.Size(112, 20);
+            this.txtMatrixM.TabIndex = 37;
+            this.txtMatrixM.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMatrixM);
+            this.Controls.Add(this.txtMatrixN);
+            this.Controls.Add(this.dgvMatrix);
+            this.Controls.Add(this.btnGenerateMatrix);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatrixN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatrixM)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerateMatrix;
+        private System.Windows.Forms.DataGridView dgvMatrix;
+        private System.Windows.Forms.NumericUpDown txtMatrixN;
+        private System.Windows.Forms.NumericUpDown txtMatrixM;
     }
 }
 
--- TpSuperior/Form1.cs
+++ TpSuperior/Form1.cs
@@ -18,12 +18,40 @@ namespace TpSuperior
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvMatrix.Rows.Clear();
+            dgvMatrix.Columns.Clear();
+            dgvMatrix.Refresh();
+            decimal n = txtMatrixN.Value;
+            decimal m = txtMatrixM.Value;
+            for (int j = 0; j < m; ++j)
+            {
+                dgvMatrix.Columns.Add("X" + dgvMatrix.Columns.Count, "X" + dgvMatrix.Columns.Count);
+            }
+            dgvMatrix.Columns.Add("TI" ,"Terminos independientes");
+            for (int i = 0; i < n; ++i)
+            {
+                dgvMatrix.Rows.Add();
+            }
+        }
+
+        private void matrix_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtMatrixN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
         }
