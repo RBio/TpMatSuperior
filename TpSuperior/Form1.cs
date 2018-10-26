d0f7ead2ecfb6e7414de97e3701cb77bc9c28eaa
RBio <bio.ronan@gmail.com>
2018-10-26 19:35:54 -0300

    Base metodo Jacobi

--- TpSuperior/Form1.cs
+++ TpSuperior/Form1.cs
@@ -14,6 +14,9 @@ namespace TpSuperior
 {
     public partial class Form1 : Form
     {
+        string method = "Jacobi";
+        string matrixType = "diagonal";
+        Matrix<double> solutionVector;
         public Form1()
         {
             InitializeComponent();
@@ -62,11 +65,23 @@ namespace TpSuperior
 
         private void btnContinuar_Click(object sender, EventArgs e)
         {
-            Matrix<double> m;
-            if (!matrixFull())
-                MessageBox.Show("Complete la matriz para continuar");
-            else
+            Matrix<double> T, r, C, m;
+            if (matrixFullAndDiagonal())
+            {
+               r = getIC();
                m = buildMatrix();
+               if (method == "Jacobi")
+                {
+                    T = jacobiTMatrix(m);
+                    C = jacobiCMatrix(m);
+                }
+                else
+                {
+                    //T = GSTMatrix(m);
+                    //C = GSCMatrix(m);
+                }
+                //solutionVector = T * r + C;
+            }
         }
 
         private void cmbMetodo_SelectedIndexChanged(object sender, EventArgs e)
@@ -84,16 +99,76 @@ namespace TpSuperior
         }
         private double getDvgMatrixValue(int i, int j)
         {
-            return Convert.ToDouble(dgvMatrix.Rows[i].Cells[j].Value.ToString());
+            return Convert.ToDouble(dgvMatrix.Rows[i].Cells[j].Value);
         }
-        private bool matrixFull()
+        private bool matrixFullAndDiagonal()
         {
             for (int i = 0; i < dgvMatrix.RowCount; ++i)
                 for (int j = 0; j < dgvMatrix.ColumnCount; ++j)
                     if (dgvMatrix.Rows[i].Cells[j].Value == null)
+                    {
+                        MessageBox.Show("Complete la matriz para continuar");
+                        return false;
+                    }
+                    else if (i == j && !rowMax(i, j))
                         return false;
 
             return true;
+        }
+        private Matrix<double> getIC()
+        {
+            return Matrix<double>.Build.Dense(dgvMatrix.RowCount, 1, (i, j) => getDvgMatrixValue(i, dgvMatrix.ColumnCount - 1));
+        }
+        private bool rowMax(int i, int j)
+        {
+            double testValue, testMax;
+            for (int k = 0; k < dgvMatrix.ColumnCount; ++k)
+            {
+                if (k == j)
+                    continue;
+                testValue = Convert.ToDouble(dgvMatrix.Rows[i].Cells[k].Value);
+                testMax = Convert.ToDouble(dgvMatrix.Rows[i].Cells[j].Value);
+                if (matrixType == "diagonal")
+                {
+                    if (testValue >= testMax)
+                    {
+                        MessageBox.Show("La matriz no es diagonal");
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (testValue > testMax)
+                    {
+                        MessageBox.Show("La matriz no es estrictamente diagonal");
+                        return false;
+                    }
+                }
+                    
+            }
+            return true;
+        }
+        private Matrix<double> jacobiTMatrix(Matrix<double> m)
+        {
+            return reciprocalMatrix(diagonalMatrix(m)) * (m.LowerTriangle() + m.UpperTriangle()); 
+        }
+        private Matrix<double> diagonalMatrix(Matrix<double> m)
+        {
+            Matrix<double> a = Matrix<double>.Build.Dense(m.RowCount, m.ColumnCount);
+            a.SetDiagonal(m.Diagonal());
+            return a;
+        }
+        private Matrix<double> reciprocalMatrix(Matrix<double> m)
+        {
+            return Matrix<double>.Build.Dense(dgvMatrix.RowCount, dgvMatrix.ColumnCount - 1, (i, j) => 1/getMatrixValue(i, j, m));
+        }
+        private double getMatrixValue(int i, int j, Matrix<double> m)
+        {
+            return Convert.ToDouble(m.Row(i).At(j)); //Check this "At"
+        }
+        private Matrix<double> jacobiCMatrix(Matrix<double> m)
+        {
+            return reciprocalMatrix(diagonalMatrix(m)) * getIC();
         }
     }
 }
